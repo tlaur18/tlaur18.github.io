76af1cc912105fda2d5fab56b2bcabe8a912fdf0
tlaur18 <thomassl@hotmail.dk>
2021-09-10 23:45:33 +0200

    Extended version

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,43 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent {
 
-  public playerX = 3;
-	public walkableTiles = ["➖","🌉","⬆️","⬇️","🏁"];
-  public gameOver = false;
+  public playerX = 8;
+  public playerEmoji = "🤓";
+  public walkableTiles = ["➖", "🌉", "⬆️", "⬇️", "🏁", "🌊", "🕶"];
+  public gameWon = false;
+  public gameLost = false;
   public tiles: Array<{ index: number, emoji: string }>;
 
-  constructor() { 
-    let s = ["⛔️","➖","➖","➖","➖","➖","➖","➖","➖","⬆️","➖","➖","➖","➖","➖","➖",
-              "🌊","🌊","🌊","🌊","🌊","🌊","🌊","🌊","➖","➖","➖","➖","➖","🏁","➖","⛔️"];
-    this.tiles = []; 
-    for (let i=0; i<s.length; i++) {
+  constructor() {
+    let s = ["⛔️", "➖", "🏁", "➖", "🌞", "➖", "➖", "➖", "➖", "➖", "➖", "➖", "⬆️", "➖", "➖", "➖",
+      "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "🌊", "➖", "➖", "➖", "➖", "➖", "🕶", "➖", "⛔️"];
+    this.tiles = [];
+    for (let i = 0; i < s.length; i++) {
       this.tiles.push({ index: i, emoji: s[i] });
     }
   }
 
   public left() {
-		let newX = this.playerX - 1;
-		if (this.walkableTiles.indexOf(this.tiles[newX].emoji) != -1) {
+    let newX = this.playerX - 1;
+    if (this.walkableTiles.indexOf(this.tiles[newX].emoji) != -1) {
       this.playerX = newX;
-      if (this.tiles[this.playerX].emoji == '🏁') this.gameOver = true;
-		}
+      this.check_collision();
+    }
   }
 
   public right() {
-		let newX = this.playerX + 1;
-		if (this.walkableTiles.indexOf(this.tiles[newX].emoji) != -1) {
+    let newX = this.playerX + 1;
+    if (this.walkableTiles.indexOf(this.tiles[newX].emoji) != -1) {
       this.playerX = newX;
-      if (this.tiles[this.playerX].emoji == '🏁') this.gameOver = true;
-		}
+      this.check_collision();
+    }
   }
 
-	public jump() {
-		if (this.tiles[this.playerX].emoji == '⬆️') {
-      this.tiles[this.playerX] = { index: this.playerX, emoji: '⬇️' };
-      for (let i=0; i<this.tiles.length; i++) {
+  public check_collision() {
+    if (this.tiles[this.playerX].emoji == '🏁') this.gameWon = true;
+    else if (this.tiles[this.playerX].emoji == '🌊') { this.gameLost = true;  this.playerEmoji = "🤕"; }
+    else if (this.tiles[this.playerX].emoji == '🕶') { this.playerEmoji = "😎"; this.walkableTiles.push("🌞"); }
+  }
+
+  public jump() {
+    // Going up
+    if (this.tiles[this.playerX].emoji == '⬆️') {
+      for (let i = 0; i < this.tiles.length; i++) {
         if (this.tiles[i].emoji == '🌊') this.tiles[i].emoji = '🌉';
+        if (this.tiles[i].emoji == '⬆️') this.tiles[i].emoji = '⬇️';
       }
-		}
+    }
+    // Going down
+    else if (this.tiles[this.playerX].emoji == '⬇️') {
+      for (let i = 0; i < this.tiles.length; i++) {
+        if (this.tiles[i].emoji == '🌉') this.tiles[i].emoji = '🌊';
+        if (this.tiles[i].emoji == '⬇️') this.tiles[i].emoji = '⬆️';
+      }
+    }
   }
 }
 
